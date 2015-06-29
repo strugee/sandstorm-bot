ddfb776e467d83e550cfe2b01af900f5cfcc6668
Alex Jordan <alex@strugee.net>
2015-06-29 00:06:37 -0700

    Build a list of webapp packages

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var spawn = require('child_process').spawn;
+var getRawBody = require('raw-body');
+
+// Assumes that debtags is installed and updated regularly
+
+var debtags_process = spawn('debtags', ['search', 'interface::web']);
+
+getRawBody(debtags_process.stdout, function(err, data) {
+	if (err) throw err;
+	var arr = [];
+	data.toString().split('\n').forEach(function(line) {
+		if (line === '') return;
+		arr.push(line.split(' - ')[0]);
+	});
+});
